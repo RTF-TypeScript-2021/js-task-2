e9ce1251f48472312015b3e5f35cc128896d98b9
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-09-22 11:41:36 +0500

    поработать над проверкой входных параметров

--- topic-2/task-2/index.js
+++ topic-2/task-2/index.js
@@ -22,7 +22,7 @@ const arrayStripped = (array, size) => {
     }
     let result = [];
     for(let i = 0; i < array.length; i+=size) { 
-        result.push(array.slice(i,i+size));
+        result.push(array.slice(i, i+size));
     }
 
     return result;
