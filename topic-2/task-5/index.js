ae3b3355cb1723645bf800a9b94a61602429c398
garche <archersve@gmail.com>
2021-09-22 10:28:18 +0500

    просто пятый файл на всякий случай

--- topic-2/task-5/index.js
+++ topic-2/task-5/index.js
@@ -6,7 +6,7 @@ import {miniMathLib} from "../task-4";
  * Верните фукнкцию, которая использьзует число Пи с точностью до 5 знаков, после запятой.
  * Модифицировать miniMathLib из прошлой задачи - запрещено
  */
-function accurateAreaCalc() {
+function accurateAreaCalc() { //может нужно новую версию файла закинуть
     miniMathLib.pi = 3.14159;
 
     return miniMathLib.circleArea;
