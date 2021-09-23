9ead26f5744cfb7aeabc7f1261efa6e7ab46210b
onezuppi <52073821+onezuppi@users.noreply.github.com>
2021-09-23 20:10:02 +0500

    Function "volume" was transferred

--- topic-2/task-6/index.js
+++ topic-2/task-6/index.js
@@ -9,19 +9,22 @@
  * Реализуйте функцию, которая возвращает модифицированный volume из miniMathLibExtended
  * в которую передается значение Пи, необходимое для расчетов
  * */
+
+const calculateVolume = function (radius, height){
+    const isCorrectParam = (param) => Number.isFinite(param) && param >= 0;
+    if (!isCorrectParam(radius)) {
+        throw new Error('Argument error. Argument radius must be a not negative number.');
+    }
+    if (!isCorrectParam(height)) {
+        throw new Error('Argument error. Argument height must be a not negative number.');
+    }
+
+    return height * this.pi * radius * radius;
+}
+
 const miniMathLibExtended = {
     pi: 3.14,
-    volume: function (radius, height){
-        const isCorrectParam = (param) => Number.isFinite(param) && param >= 0;
-        if (!isCorrectParam(radius)) {
-            throw new Error('Argument error. Argument radius must be a not negative number.');
-        }
-        if (!isCorrectParam(height)) {
-            throw new Error('Argument error. Argument height must be a not negative number.');
-        }
-
-        return height * this.pi * radius * radius;
-    }
+    volume: calculateVolume
 }
 
 function cylinderVolumeAccurate(pi){
