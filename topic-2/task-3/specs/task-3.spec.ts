6f126e3643379decb5c03413059de601abf265a1
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-09-25 11:06:58 +0500

    added test

--- topic-2/task-3/specs/task-3.spec.ts
+++ topic-2/task-3/specs/task-3.spec.ts
@@ -6,4 +6,5 @@ test('Method works fine', () => {
     expect(isGroup("[({})](]")).toBe(false);
     expect(isGroup("([{}])")).toBe(true);
     expect(isGroup("(}")).toBe(false);
+    expect(isGroup("())")).toBe(false);
 });
