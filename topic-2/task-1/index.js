5ec3e9256d7db87e3bf5cc84b2f74651412d228a
alexeyvolhin <82944068+alexeyvolhin@users.noreply.github.com>
2021-09-26 10:31:36 +0500

    Task 1 + config fixes

--- topic-2/task-1/index.js
+++ topic-2/task-1/index.js
@@ -9,9 +9,17 @@
  * @returns массив с удаленным элементом
  */
 function removeSmallest(numbers) {
-    
+    const lowest = Math.min(...numbers);
+    if (numbers.length > 0) {
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i] == lowest) {
+                numbers.splice(i,1);
+
+                return numbers;
+            }
+        }
+    } else {
+        return numbers;
+    }
 }
-
 module.exports.removeSmallest = removeSmallest;
-
-
