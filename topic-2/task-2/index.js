a7b094f61d65c1fb5b13b7162f3146609f4c4ee9
alexeyvolhin <82944068+alexeyvolhin@users.noreply.github.com>
2021-09-26 18:54:33 +0500

    Task 2

--- topic-2/task-2/index.js
+++ topic-2/task-2/index.js
@@ -7,15 +7,27 @@
  * arrayStripped(['a', 'b', 'c', 'd'], 2)) => [['a', 'b'], ['c', 'd']]
  * arrayStripped([1, 'b', 5, 0, 'c'], 2)) => [[1, 'b'], [5, 0], ['c']]
  * 
- * Если разбивать на группы нечего, выведите пустой массив.
+ * Если разбивать на группы нечего, выведите пустой массив.1
  * 
  * @param {*} array массив на вход
  * @param {*} size из скольких элементов состоит группа
  * @returns массив разбитый на группы
 */
+
 const arrayStripped = (array, size) => {
+    const result = [];
+    if (typeof(size) !== "number" || size <= 0 || array === null) {
+        throw new Error ("Invalid data");
+    } else if (array === undefined || typeof(array) !== "object") {
+        return result;
+    } else {
+        const result1 = [];
+        while (array.length) {
+            result1.push(array.splice(0, size));
+        }
 
+        return result1;
+    }
 }
-
 
 module.exports.arrayStripped = arrayStripped;
