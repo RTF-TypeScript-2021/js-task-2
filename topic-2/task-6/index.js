4b52c2ce5701cb20c703a587cf2a43c98729d4b2
DESKTOP-7NFU6LQ\Пользователь <utevasvetlana31@gmail.com>
2021-09-27 01:45:25 +0500

    task 6 done

--- topic-2/task-6/index.js
+++ topic-2/task-6/index.js
@@ -9,11 +9,18 @@
  * Реализуйте функцию, которая возвращает модифицированный volume из miniMathLibExtended
  * в которую передается значение Пи, необходимое для расчетов
  * */
-const miniMathLibExtended = {
+ const miniMathLibExtended = {
+    pi: 3.14,
+    volume: function (radius, height) {
+        if (radius <= 0 || height <= 0) {
+            throw new Error('Negative data');
+        }
+        return this.pi * radius * radius * height;
+    }
 }
-
-function cylinderVolumeAccurate(pi){
     
+function cylinderVolumeAccurate(pi) {
+    return miniMathLibExtended.volume.bind({'pi' : pi });
 }
 
 module.exports.miniMathLibExtended = miniMathLibExtended;
