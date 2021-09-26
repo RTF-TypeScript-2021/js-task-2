f2bf6af909ad0d0149b43f7aedb65c3c89be5889
Nikita Zernyukov <n.zernyukov@artsofte.ru>
2021-09-26 11:30:26 +0500

    Revert "js-task-3"
    This reverts commit dc4ac4fe54168ac20cf213076c34a9c98fd9a67a.

--- topic-3/task-4/specs/task-4.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Organaizer } from "..";
-
-test('Создает объект класса Organaizer', () => {
-	const organaizer = new Organaizer([], []);
-	expect(organaizer.meetings).toStrictEqual([]);
-	expect(organaizer.vacations).toStrictEqual([]);
-});
