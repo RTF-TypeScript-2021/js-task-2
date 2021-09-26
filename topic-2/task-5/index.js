f41dc9042a9a7ff43bee8044b3bfc74a2cf6c393
alexeyvolhin <82944068+alexeyvolhin@users.noreply.github.com>
2021-09-26 21:11:36 +0500

    Task 5 try

--- topic-2/task-5/index.js
+++ topic-2/task-5/index.js
@@ -1,14 +1,17 @@
-import {miniMathLib} from "../task-4";
-
 /**
  * Задача 5 - Нехватка точности
  * Оказалось, что ранее написанная вами функция недостаточно точна : (
  * Верните фукнкцию, которая использьзует число Пи с точностью до 5 знаков, после запятой.
  * Модифицировать miniMathLib из прошлой задачи - запрещено
  */
-function accurateAreaCalc() {
-    
-}
 
+import {miniMathLib} from "../task-4/index.js";
+function accurateAreaCalc() {
+    const tempP = {
+        p : (Math.PI).toFixed(5)
+    }
+
+    return miniMathLib.circleArea.bind(tempP);
+}
 
 module.exports.accurateAreaCalc = accurateAreaCalc
