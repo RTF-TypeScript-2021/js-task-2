47d3baea6b5ca70561d35d2bf2dffacf24541c6f
DESKTOP-7NFU6LQ\Пользователь <utevasvetlana31@gmail.com>
2021-09-27 01:39:23 +0500

    task 1 done

--- topic-2/task-1/index.js
+++ topic-2/task-1/index.js
@@ -8,9 +8,29 @@
  * @param {*} numbers массив
  * @returns массив с удаленным элементом
  */
-function removeSmallest(numbers) {
-    
-}
+ function removeSmallest(numbers) {
+    let resultArray = [];
+    if (!Array.isArray(numbers)) {
+        throw new Error('Invalid array');
+    }
+    let minIndex = 0;
+    let minElement = numbers[minIndex];
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (typeof numbers[i] === 'number') {
+            if (numbers[i] < minElement) {
+                minElement = numbers[i];
+                minIndex = i;
+            }
+        }
+        else {
+            throw new Error ('Array is not an array of numbers')
+        }
+    };
+    if (numbers.length === 0) {
+        return resultArray;
+    }
+    return numbers.filter((arr, index) => index !== minIndex);
+    }
 
 module.exports.removeSmallest = removeSmallest;
 
