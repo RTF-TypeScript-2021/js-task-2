f463a752f63d228146cae9e8dd25ee7aeb017979
Sychqh <sychqh@gmail.com>
2021-09-27 19:35:43 +0500

    Сделал второе задание

--- topic-2/task-2/index.js
+++ topic-2/task-2/index.js
@@ -14,7 +14,29 @@
  * @returns массив разбитый на группы
 */
 const arrayStripped = (array, size) => {
+    if (array === null){
+        throw new Error("Array can't be null");
+    }
+    if (typeof(size) !== "number") {
+        throw new Error("Size has to a number");
+    }
+    if (!Array.isArray(array)) {
+        return [];
+    }
 
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i += size){
+        const groupArray = [];
+
+        for (let j = i; j < i + size && j < array.length; j++) {
+            groupArray.push(array[j]);
+        }
+
+        newArray.push(groupArray);
+    }
+
+    return newArray;
 }
 
 
